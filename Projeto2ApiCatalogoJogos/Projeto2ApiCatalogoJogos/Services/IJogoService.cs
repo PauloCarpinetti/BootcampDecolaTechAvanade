79e23a97604f0adc7d79f2d12d7d20c8bc424df3
PauloCarpinetti <74799303+PauloCarpinetti@users.noreply.github.com>
2021-10-17 22:31:08 -0300

    Add files via upload

---
+++ Projeto2ApiCatalogoJogos/Projeto2ApiCatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,21 @@
+﻿using Projeto2ApiCatalogoJogos.InputModel;
+using Projeto2ApiCatalogoJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto2ApiCatalogoJogos.Services
+{
+    public interface IJogoService : IDisposable
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid Id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid Id, JogoInputModel jogo);
+        Task Atualizar(Guid Id, double preco);
+        Task AtualizarNome(Guid Id, string nome);
+        Task Remover(Guid Id);
+
+    }
+}
